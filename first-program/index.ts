4c1e7ecdfbe4de39706628120b03a485a1ae68c2
smmunna <minhazulabedinmunna@gmail.com>
2023-11-06 23:56:58 +0600

    updated

--- first-program/index.ts
+++ first-program/index.ts
@@ -1,5 +1,21 @@
+// Simple program
 let a = "munna";
 console.log(a)
 
-const b = 12;
-console.log(b)
+//Types
+/**
+ * 1. String types
+ * 2. Number types
+ * */ 
+
+let fname:string
+fname = 'ok'
+// console.log(fname)
+
+const fetchData = async()=>{
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json()
+    console.log(data)
+}
+
+fetchData()
